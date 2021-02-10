f8ab943fdbb2abf5e476c8347eabf903acf212f8
Eric Lechner <ericl@xilinx.com>
2021-02-10 10:29:10 -0800

    removing some console output, remove some commented-out code

--- ced/Xilinx/IPI/extensible_platform/init.tcl
+++ ced/Xilinx/IPI/extensible_platform/init.tcl
@@ -33,14 +33,6 @@ proc addOptions {DESIGNOBJ PROJECT_PARAM.BOARD_PART} {
     lappend x [dict create name "Include_LPDDR" type "bool" value "false" enabled true]
     lappend x [dict create name "Clock_Options" type "string" value "clk_out1 200.000 0 true clk_out2 100.000 1 false" enabled true]
     lappend x [dict create name "IRQS" type "string" value "32" value_list {"0 No_interrupts" "32 32_Interrupts,_single_interrupt_controller" "63 63_interrupts,_cascaded_interrupt_controller"} enabled true]
-    #set components [ get_board_components -of_objects [get_boards ${PROJECT_PARAM.BOARD_PART}] ]
-    #get list of board components based on board part
-    #set value {}
-    #foreach { comp } $components {
-    #    lappend value $comp
-    #    lappend value "false"
-    #}
-    #lappend x [dict create name "IP_Options" type "string" value $value enabled true]
     return $x
 }
 
@@ -53,22 +45,12 @@ proc addGUILayout {DESIGNOBJ PROJECT_PARAM.BOARD_PART} {
 
     ced::add_param -name IRQS -display_name "Interrupts" -parent $page -designObject $designObj -widget radioGroup
 
-    puts "PROJECT_PARAM.BOARD_PART: ${PROJECT_PARAM.BOARD_PART}"
-
-    set ddr [ced::add_group -name "Versal LPDDR Configurations" -display_name "Versal LPDDR Configurations"  -parent $page -visible true -designObject $designObj ]
-    ced::add_param -name Include_LPDDR -display_name "Include LPDDR" -parent $ddr -designObject $designObj -widget checkbox
-
-    #set ip [ced::add_group -name "Board_Components" -display_name "Board Components"  -parent $page -visible true -designObject $designObj ]
-    #ced::add_custom_widget -name widget_IP -hierParam IP_Options -class_name PlatformIPWidget -parent $ip $designObj
-
-    #set imageVar [ced::add_image -name Image -parent $ddr -designObject $designObj -width 500 -height 300 -layout vertical]
-
+    set ddr [ced::add_group -name "Versal LPDDR Configurations" -display_name "Memory"  -parent $page -visible true -designObject $designObj ]
+    ced::add_param -name Include_LPDDR -display_name "Include LPDDR interfaces" -parent $ddr -designObject $designObj -widget checkbox
 }
 
 # validater { parameters_used } { parameters_modified} { functionality }
 validater { Clock_Options.VALUE } { Clock_Options.ERRMSG } {
-    puts "XXX validate: clock options = ${Clock_Options.VALUE}"
-
     set clk_options ${Clock_Options.VALUE}
     set clk_ports {}
     set clk_freqs {}
